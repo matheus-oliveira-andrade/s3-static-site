b2be5c4e9ba59b47b94f4a5f3898b2ac1c5113c8
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2022-10-20 23:00:21 -0300

    feat: add ga

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,24 @@ export default function Home() {
         <meta name="twitter:card" content="summary"></meta>
         <meta name="twitter:title" content={title}></meta>
         <meta name="twitter:description" content={description}></meta>        
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
 
       <main>
