d2e3510b5aeed95282cd559f7f69768ac8d74173
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2022-11-03 23:32:16 -0300

    feat: update linkedin url (#7)

--- site/pages/index.js
+++ site/pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://www.linkedin.com/in/matheus-oliveira-479467162/"
+            href="https://www.linkedin.com/in/matheus-oliveira-andrade"
             className="card"
           >
             <img
