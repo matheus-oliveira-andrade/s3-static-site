641e1432212afdc8bd080c1eb14e2fc373ac6fda
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2022-12-27 21:59:50 -0300

    refactor: change image name

--- site/start-dev.sh
+++ site/start-dev.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build -t blog:latest -f Dockerfile.dev .
+docker build -t blog:dev -f Dockerfile.dev .
 
-docker run -p 3000:3000 -v $(pwd):/app blog
+docker run -p 3000:3000 -v $(pwd):/app blog:dev
